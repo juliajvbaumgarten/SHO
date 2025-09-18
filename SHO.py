c1d11b1ed1a38ff9f3fc5f4c1b28d4bcd342ca80
juliajvbaumgarten <juliaxbaumgarten@gmail.com>
2025-09-18 13:35:39 -0400

    Update SHO.py
    Initial set-up

--- SHO.py
+++ SHO.py
@@ -11,3 +11,24 @@ Then the ODE system is:
 dx/dt = v
 dv/dt = -(k/m) x
 """
+
+# ---------- Import modules ---------- 
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ----------- Parameters -------------
+m = 1.0 # mass [kg]
+k = 4.0 # Spring [N/m] (omega = sqrt(k/m) = 2 rad/s)
+omega = math.sqrt(k/m)
+
+# ------------------------------------
+dt = 0.01 # timestep [s]
+T = 20.0 # total time [s]
+n_steps = int(T/dt)
+
+# ------ Initial Conditions ---------
+x0 = 1.0
+v0 = 0.5
+
+
